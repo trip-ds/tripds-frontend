333444741fd78da56b29b8ff1e2fc7e114fa4bac
jijongkwon <help3451@naver.com>
2024-05-19 22:52:37 +0900

    Feat(UserRegister) : 회원가입 메서드 추가

--- src/components/users/UserRegister.vue
+++ src/components/users/UserRegister.vue
@@ -1,75 +1,109 @@
 <script setup>
-import { RouterLink } from "vue-router";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { useMemberStore } from "@/stores/member.js";
+
+const router = useRouter();
+const memberStore = useMemberStore();
+
+const nickname = ref("");
+const age = ref(null);
+const email = ref("");
+const password = ref("");
+const confirmPassword = ref("");
+const address = ref("");
+
+const regist = async () => {
+  if (password.value !== confirmPassword.value) {
+    alert("비밀번호 확인이 일치하지 않습니다.");
+    return;
+  }
+
+  const registerUser = {
+    nickname: nickname.value,
+    age: age.value,
+    email: email.value,
+    password: password.value,
+    address: address.value,
+  };
+
+  try {
+    await memberStore.userSignup(registerUser);
+    router.push("/"); 
+  } catch (error) {
+    alert(error.response.data.message);
+  }
+};
 </script>
 
 <template>
-  <div class="container">
-    <div class="form-container">
-      <div class="form-container">
-        <p class="title">회원가입</p>
-        <p class="sub-title">트립동산에 서비스를 이용하실 수 있습니다.</p>
-        <form class="form">
-          <input type="text" class="input" placeholder="닉네임" />
-          <input type="number" class="input" placeholder="나이" />
-          <input type="email" class="input" placeholder="이메일" />
-          <input type="password" class="input" placeholder="비밀번호" />
-          <input type="password" class="input" placeholder="비밀번호 확인" />
-          <input type="text" class="input" placeholder="주소" />
-          <button class="form-btn">회원가입</button>
-        </form>
-        <p class="sign-up-label">
-          이미 계정이 있으십니까 ?
-          <span class="sign-up-link">
-            <RouterLink :to="{ name: 'user-login' }" class="link">로그인</RouterLink>
-          </span>
-          <span> / </span>
-          <span class="sign-up-link">
-            <RouterLink :to="{ name: 'main' }" class="link">홈으로</RouterLink>
-          </span>
-        </p>
-        <div class="buttons-container">
-          <div class="google-login-button">
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              stroke-width="0"
-              version="1.1"
-              x="0px"
-              y="0px"
-              class="google-icon"
-              viewBox="0 0 48 48"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill="#FFC107"
-                d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12
-      c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24
-      c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
-              ></path>
-              <path
-                fill="#FF3D00"
-                d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657
-      C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
-              ></path>
-              <path
-                fill="#4CAF50"
-                d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36
-      c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
-              ></path>
-              <path
-                fill="#1976D2"
-                d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571
-      c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
-              ></path>
-            </svg>
-            <span style="font-family: 'Jalnan2TTF'">Sign up with Google</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+ <div class="container">
+   <div class="form-container">
+     <div class="form-container">
+       <p class="title">회원가입</p>
+       <p class="sub-title">트립동산에 서비스를 이용하실 수 있습니다.</p>
+       <form class="form">
+         <input type="text" v-model="nickname" class="input" placeholder="닉네임" />
+         <input type="number" v-model="age" class="input" placeholder="나이" />
+         <input type="email" v-model="email" class="input" placeholder="이메일" />
+         <input type="password" v-model="password" class="input" placeholder="비밀번호" />
+         <input type="password" v-model="confirmPassword" class="input" placeholder="비밀번호 확인" />
+         <input type="text" v-model="address" class="input" placeholder="주소" />
+         <button class="form-btn" @click.prevent="regist">회원가입</button>
+       </form>
+       <p class="sign-up-label">
+         이미 계정이 있으십니까 ?
+         <span class="sign-up-link">
+           <RouterLink :to="{ name: 'user-login' }" class="link">로그인</RouterLink>
+         </span>
+         <span> / </span>
+         <span class="sign-up-link">
+           <RouterLink :to="{ name: 'main' }" class="link">홈으로</RouterLink>
+         </span>
+       </p>
+       <div class="buttons-container">
+         <div class="google-login-button">
+           <svg
+             stroke="currentColor"
+             fill="currentColor"
+             stroke-width="0"
+             version="1.1"
+             x="0px"
+             y="0px"
+             class="google-icon"
+             viewBox="0 0 48 48"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+           >
+             <path
+               fill="#FFC107"
+               d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12
+     c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24
+     c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+             ></path>
+             <path
+               fill="#FF3D00"
+               d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657
+     C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+             ></path>
+             <path
+               fill="#4CAF50"
+               d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36
+     c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+             ></path>
+             <path
+               fill="#1976D2"
+               d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571
+     c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+             ></path>
+           </svg>
+           <span style="font-family: 'Jalnan2TTF'">Sign up with Google</span>
+         </div>
+       </div>
+     </div>
+   </div>
+ </div>
 </template>
 
 <style scoped>
