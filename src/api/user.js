6d8948f3d5040ec1e7c96ef00557ffb0f3278a0c
jijongkwon <help3451@naver.com>
2024-05-19 19:19:33 +0900

    refactor(user) : url 변경

--- src/api/user.js
+++ src/api/user.js
@@ -9,16 +9,16 @@ async function userConfirm(param, success, fail) {
 
 async function findById(email, success, fail) {
   local.defaults.headers["Authorization"] = sessionStorage.getItem("accessToken");
-  await local.get(`api/user/info/${email}`).then(success).catch(fail);
+  await local.get(`/api/user/info/${email}`).then(success).catch(fail);
 }
 
 async function tokenRegeneration(user, success, fail) {
   local.defaults.headers["refreshToken"] = sessionStorage.getItem("refreshToken"); //axios header에 refresh-token 셋팅
-  await local.post(`/user/refresh`, user).then(success).catch(fail);
+  await local.post(`/api/user/refresh`, user).then(success).catch(fail);
 }
 
-async function logout(userid, success, fail) {
-  await local.get(`/user/logout/${userid}`).then(success).catch(fail);
+async function logout(email, success, fail) {
+  await local.get(`/api/user/logout/${email}`).then(success).catch(fail);
 }
 
 export { userConfirm, findById, tokenRegeneration, logout };
