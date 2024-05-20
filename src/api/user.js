27e555de203082fd0aa119dd4cd7168a9e441709
jijongkwon <help3451@naver.com>
2024-05-20 13:29:01 +0900

    Feat(user) : 유저 업데이트 api 선언

--- src/api/user.js
+++ src/api/user.js
@@ -4,15 +4,20 @@ import BaseComponent from "bootstrap/js/dist/base-component";
 const local = localAxios();
 
 async function userRegister(param, success, fail) {
-  console.log(param)
+  console.log(param);
   await local.post(`/api/user/signup`, param).then(success).catch(fail);
+}
+
+async function userUpdate(param, success, fail) {
+  console.log(param);
+  await local.put(`/api/user/update`, param).then(success).catch(fail);
 }
 
 async function userConfirm(param, success, fail) {
   await local.post(`/api/user/login`, param).then(success).catch(fail);
 }
 
-async function findById(email, success, fail) {
+async function findByEmail(email, success, fail) {
   local.defaults.headers["Authorization"] = sessionStorage.getItem("accessToken");
   await local.get(`/api/user/info/${email}`).then(success).catch(fail);
 }
@@ -26,4 +31,4 @@ async function logout(email, success, fail) {
   await local.get(`/api/user/logout/${email}`).then(success).catch(fail);
 }
 
-export { userRegister, userConfirm, findById, tokenRegeneration, logout };
+export { userRegister, userUpdate, userConfirm, findByEmail, tokenRegeneration, logout };
