f0f1f8d532bc284c2b34006dccc04f2e261ada62
jijongkwon <help3451@naver.com>
2024-05-20 13:30:47 +0900

    Feat(userMenu) : 유저 상태 동적으로 처리

--- src/stores/userMenu.js
+++ src/stores/userMenu.js
@@ -4,12 +4,13 @@ import { defineStore } from "pinia";
 import { useMemberStore } from "@/stores/member.js";
 
 export const useUserMenuStore = defineStore("menuStore", () => {
+  const memberStore = useMemberStore();
+
   const menuList = ref([
-    { name: "회원가입", routeName: "user-join", show: true },
-    { name: "로그인", routeName: "user-login", show: true },
-    // 임시
-    { name: "내 정보", routeName: "user-mypage", show: false },
-    { name: "로그아웃", routeName: "user-logout", show: false },
+    { name: "회원가입", routeName: "user-join", show: !memberStore.isLogin },
+    { name: "로그인", routeName: "user-login", show: !memberStore.isLogin },
+    { name: "내 정보", routeName: "user-mypage", show: memberStore.isLogin },
+    { name: "로그아웃", routeName: "user-logout", show: memberStore.isLogin },
   ]);
 
   const changeMenuState = () => {
