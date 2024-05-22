e93b3b5aebd55ca5a33a4bee720bebcf14e637f4
jijongkwon <help3451@naver.com>
2024-05-22 17:06:03 +0900

    Feat(MainView) : aside 추가

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,10 +1,21 @@
 <script setup>
+import { ref } from "vue";
 import HeadingNavbar from "@/components/layout/HeadingNavbar.vue";
+import MyInterestEstateAside from "@/components/estate/MyInterestEstateAside.vue";
+
+const isAsideOpen = ref(false);
+
+const toggleAside = () => {
+  console.log(isAsideOpen.value);
+  isAsideOpen.value = !isAsideOpen.value;
+};
 </script>
 
 <template>
-  <HeadingNavbar />
+  <HeadingNavbar @toggle-aside="toggleAside" />
   <div>main</div>
+
+  <MyInterestEstateAside :isOpen="isAsideOpen" @close="isAsideOpen = false" />
 </template>
 
 <style scoped></style>
