cadcf8e1f39c89d6dd917212e50ce2e318d450bf
jijongkwon <help3451@naver.com>
2024-05-23 10:02:34 +0900

    Feat : 관심 부동산 목록 등록하기

--- src/api/estate.js
+++ src/api/estate.js
@@ -2,9 +2,15 @@ import { localAxios } from "@/util/http-commons";
 
 const local = localAxios();
 
+// 관심 부동산 //
+async function insertEstateInterest(param, success, fail) {
+  console.log(param);
+  await local.post(`/api/estate/interest/insert`, param).then(success).catch(fail);
+}
+
 async function getEstateInterestInfoByEmail(email, success, fail) {
   console.log(email);
   await local.get(`/api/estate/interest/info/${email}`).then(success).catch(fail);
 }
 
-export { getEstateInterestInfoByEmail };
+export { insertEstateInterest, getEstateInterestInfoByEmail };
